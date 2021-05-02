06fbbf39d60f2e44d1a04cd68cad48b2a7092a03
cjones0429 <cejones1997@aim.com>
2021-05-02 14:37:00 -0400

    adding random destination

--- dayTripGen.js
+++ dayTripGen.js
@@ -1,1 +1,10 @@
 "use strict"
+
+//Random Day Trip Generator
+
+let listOfDestinations = ["The beach", "The mountains", "A lake"];
+let randomDestination = Math.floor(Math.random() * listOfDestinations.length);
+
+console.log(listOfDestinations[randomDestination]);
+
+

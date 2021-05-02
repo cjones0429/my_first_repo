63642611eb649f82e290b4d8f20b82493ac2fb39
cjones0429 <cejones1997@aim.com>
2021-05-02 14:38:56 -0400

    adding restaurant types

--- dayTripGen.js
+++ dayTripGen.js
@@ -7,4 +7,9 @@ let randomDestination = Math.floor(Math.random() * listOfDestinations.length);
 
 console.log(listOfDestinations[randomDestination]);
 
+let listOfRestaurantTypes = ["Italian", "American", "Mexican"];
+let randomRestuarantType = Math.floor(Math.random() * listOfRestaurantTypes.length);
+
+console.log(listOfRestaurantTypes[randomRestuarantType]);
+
 

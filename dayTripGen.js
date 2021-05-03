45c43de8f8cf55d992327eb5ad87e1394d97676e
cjones0429 <cejones1997@aim.com>
2021-05-03 08:03:04 -0400

    added functions to get destination, restaurant types, modes of transportation, and forms of entertainment

--- dayTripGen.js
+++ dayTripGen.js
@@ -2,22 +2,40 @@
 
 //Random Day Trip Generator
 
-let listOfDestinations = ["The beach", "The mountains", "A lake"];
+function generateADestination(){
+
+let listOfDestinations = ["the beach", "the mountains", "a lake"];
 let randomDestination = Math.floor(Math.random() * listOfDestinations.length);
 
 console.log(listOfDestinations[randomDestination]);
+
+}
+
+function generateARestaurantType(){
 
 let listOfRestaurantTypes = ["Italian", "American", "Mexican"];
 let randomRestuarantType = Math.floor(Math.random() * listOfRestaurantTypes.length);
 
 console.log(listOfRestaurantTypes[randomRestuarantType]);
 
+}
+
+function generateATransportationType(){
+
 let listOfTransportationTypes = ["A car", "A train", "A plane"];
 let randomTransportationType = Math.floor(Math.random() * listOfTransportationTypes.length);
 
 console.log(listOfTransportationTypes[randomTransportationType]);
 
+}
+
+function generateEntertainment(){
+
 let listOfEntertainment = ["Live music", "Movie theater", "A museum"];
 let randomFormOfEntertainment = Math.floor(Math.random() * listOfEntertainment.length);
 
 console.log(listOfEntertainment[randomFormOfEntertainment]);
+
+}
+
+

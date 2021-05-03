afa1d690835ee3ae2bacf0603dc3a98b5c5ea576
cjones0429 <cejones1997@aim.com>
2021-05-03 10:18:11 -0400

    creating user prompts to decide if they like the generated option or not

--- dayTripGen.js
+++ dayTripGen.js
@@ -37,5 +37,48 @@ let randomFormOfEntertainment = Math.floor(Math.random() * listOfEntertainment.l
 console.log(listOfEntertainment[randomFormOfEntertainment]);
 
 }
+// let userReadyToStart = prompt("Are you ready to generate your trip?");
+// if(userReadyToStart === "yes"){
+//     console.log("Let's begin!");
+// }
 
+generateADestination();
+
+// let userInputDestination = "Yes"
+// userInputDestination = prompt("Are you happy with this destination? Please enter 'Yes' or 'No'");
+// while(userInputDestination !== "Yes" ||  userInputDestination === "No" || userInputDestination === "no"){
+//     console.clear();
+//     generateADestination();
+// }
+// if(userInputDestination === "Yes" || userInputDestination === "yes"){
+//     console.log("Awesome!");
+// }
+
+
+let userInputDestination = prompt("Are you happy with this destination? Please enter 'Yes' or 'No'");
+if(userInputDestination === "Yes" || userInputDestination === "yes"){
+    console.log("Awesome!")
+}
+else if(userInputDestination !== "Yes" ||  userInputDestination === "No" || userInputDestination === "no"){
+    console.clear();
+    generateADestination();
+    let userWantsFirstNewDestination = prompt("Do you like this destination? Please enter 'Yes' or 'No'");
+    if(userWantsFirstNewDestination === "Yes" || userInputDestination === "yes"){
+        console.log("Perfect!")
+    }
+    else if(userWantsFirstNewDestination !== "Yes" || userWantsFirstNewDestination === "No" || userWantsFirstNewDestination === "no")
+    console.clear();
+    generateADestination();
+    }
+
+
+//     }
+//     let userWantsSecondNewDestination = prompt("Do you like this destination? Please enter 'Yes' or 'No'");
+//     else if(userWantsSecondNewDestination)
+
+generateARestaurantType();
+
+generateATransportationType();
+
+generateEntertainment();
 

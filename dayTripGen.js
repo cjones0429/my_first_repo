9d8cb315709ab653d3e68df12ae7aab5fce97993
cjones0429 <cejones1997@aim.com>
2021-05-03 21:58:19 -0400

    fixed/changed function to let user change destination, restaurant, transportation, or entertainment

--- dayTripGen.js
+++ dayTripGen.js
@@ -37,12 +37,70 @@ let randomFormOfEntertainment = Math.floor(Math.random() * listOfEntertainment.l
 console.log(listOfEntertainment[randomFormOfEntertainment]);
 
 }
-// let userReadyToStart = prompt("Are you ready to generate your trip?");
-// if(userReadyToStart === "yes"){
-//     console.log("Let's begin!");
-// }
 
 generateADestination();
+
+generateARestaurantType();
+
+generateATransportationType();
+
+generateEntertainment();
+
+function askIfTripIsComplete(){
+let isYourTripComplete = "yes"
+let userWantsToChange
+isYourTripComplete = prompt("Is your day trip complete? Please enter 'Yes' or 'No'");
+    if(isYourTripComplete == "Yes" || isYourTripComplete == "yes"){
+    console.log("Awesome! Enjoy your trip!");
+    }
+    else if(isYourTripComplete !== "Yes" || isYourTripComplete == "no" || isYourTripComplete == "No"){
+        userWantsToChange = prompt("What option would you like to change? (Destination, restaurant, transportation, or entertainment)");
+    }
+        if(userWantsToChange == "destination"){
+            console.log("Let's change the destination!")
+            generateADestination()
+            askIfTripIsComplete();
+    }
+        else if(userWantsToChange == "restaurant"){
+            console.log("Let's change the restaurant type!")
+            generateARestaurantType()
+            askIfTripIsComplete();
+    }
+        else if(userWantsToChange == "transportation"){
+            console.log("Let's change the transportation type!")
+            generateATransportationType()
+            askIfTripIsComplete();
+    }
+        else if(userWantsToChange == "entertainment"){
+            console.log("Let's change the entertainment!")
+            generateEntertainment()
+            askIfTripIsComplete();
+    }
+}
+
+askIfTripIsComplete();
+
+
+// if(userInput == "destination" || userInput == "entertainment"){
+//     console.log("We will change the destination for you")
+//     return 
+// }
+// else if(userInput == "transportation"){
+//     console.log("We will change the transportation for you")
+// }
+
+// askUserInput()
+
+// function askUserInput(){
+    
+//     let validInput = false;
+
+//     while(validInput == false){
+//         var userInput = prompt("What option would you like changed? (Destination, resturant, transportation, entertainment)")
+
+
+
+
 
 // let userInputDestination = "Yes"
 // userInputDestination = prompt("Are you happy with this destination? Please enter 'Yes' or 'No'");
@@ -55,30 +113,27 @@ generateADestination();
 // }
 
 
-let userInputDestination = prompt("Are you happy with this destination? Please enter 'Yes' or 'No'");
-if(userInputDestination === "Yes" || userInputDestination === "yes"){
-    console.log("Awesome!")
-}
-else if(userInputDestination !== "Yes" ||  userInputDestination === "No" || userInputDestination === "no"){
-    console.clear();
-    generateADestination();
-    let userWantsFirstNewDestination = prompt("Do you like this destination? Please enter 'Yes' or 'No'");
-    if(userWantsFirstNewDestination === "Yes" || userInputDestination === "yes"){
-        console.log("Perfect!")
-    }
-    else if(userWantsFirstNewDestination !== "Yes" || userWantsFirstNewDestination === "No" || userWantsFirstNewDestination === "no")
-    console.clear();
-    generateADestination();
-    }
 
+
+// let userInputDestination = prompt("Are you happy with this destination? Please enter 'Yes' or 'No'");
+// if(userInputDestination === "Yes" || userInputDestination === "yes"){
+//     console.log("Awesome!")
+// }
+// else if(userInputDestination !== "Yes" ||  userInputDestination === "No" || userInputDestination === "no"){
+//     console.clear();
+//     generateADestination();
+//     let userWantsFirstNewDestination = prompt("Do you like this destination? Please enter 'Yes' or 'No'");
+//     if(userWantsFirstNewDestination === "Yes" || userInputDestination === "yes"){
+//         console.log("Perfect!")
+//     }
+//     else if(userWantsFirstNewDestination !== "Yes" || userWantsFirstNewDestination === "No" || userWantsFirstNewDestination === "no")
+//     console.clear();
+//     generateADestination();
+//     }
 
 //     }
 //     let userWantsSecondNewDestination = prompt("Do you like this destination? Please enter 'Yes' or 'No'");
 //     else if(userWantsSecondNewDestination)
 
-generateARestaurantType();
 
-generateATransportationType();
-
-generateEntertainment();
 
